12d11b1cd3e5aeddca60a62e49d6e383afcb1672
eduardBM <cubusbacau@yahoo.com>
2015-11-20 16:19:23 +0200

    OVS-3332 Cinder ci - add vpool

--- cinderci/dsvm-tempest-full/add_vpool.py
+++ cinderci/dsvm-tempest-full/add_vpool.py
@@ -12,15 +12,15 @@ from ovs.extensions.hypervisor.mgmtcenters.management.openstack_mgmt import Open
 
 
 pmachine = System.get_my_storagerouter().pmachine
-mgmt_center = MgmtCenter(data={'name':'Openstack',
-                               'description':'test',
-                               'username':'admin',
-                               'password':'rooter',
-                               'ip':'127.0.0.1',
-                               'port':80,
-                               'type':'OPENSTACK',
-                               'metadata':{'integratemgmt':True,
-                                           }
+mgmt_center = MgmtCenter(data={'name': 'Openstack',
+                               'description': 'test',
+                               'username': 'admin',
+                               'password': 'rooter',
+                               'ip': '127.0.0.1',
+                               'port': 80,
+                               'type': 'OPENSTACK',
+                               'metadata': {'integratemgmt': True,
+                                            }
                                })
 
 IP = check_output("""ip a l dev eth0 | grep "inet " | awk '{split($0,a," "); split(a[2],b,"/"); print(b[1])}'""", shell=True).strip()
@@ -30,31 +30,36 @@ pmachine.save()
 osm = OpenStackManagement(None)
 osm.configure_host(IP)
 for sr in StorageRouterList.get_storagerouters():
-     for disk in sr.disks:
-         for partition in disk.partitions:
-            for role in [DiskPartition.ROLES.DB, DiskPartition.ROLES.SCRUB, DiskPartition.ROLES.READ, DiskPartition.ROLES.WRITE]:
+    print('>', sr.name)
+    for disk in sr.disks:
+        print('>>', disk.name, disk.size)
+        for partition in disk.partitions:
+            print('>>>', partition.path, partition.size)
+            for role in [DiskPartition.ROLES.DB, DiskPartition.ROLES.SCRUB,
+                         DiskPartition.ROLES.READ, DiskPartition.ROLES.WRITE]:
                 partition.roles.append(role)
+            print('>>>>', partition.roles)
             partition.save()
 
-add_vpool_params = {'storagerouter_ip':IP,
+add_vpool_params = {'storagerouter_ip': IP,
                     'vpool_name': 'local',
-                    'type':'local',
+                    'type': 'local',
                     'readcache_size': 10,
                     'writecache_size': 10,
-                    'mountpoint_bfs':'/mnt/bfs',
-                    'mountpoint_temp':'/mnt/tmp',
-                    'mountpoint_md':'/mnt/md',
-                    'mountpoint_readcaches':['/mnt/cache1'],
-                    'mountpoint_writecaches':['/mnt/cache2'],
-                    'mountpoint_foc':'/mnt/cache1',
-                    'storage_ip':'127.0.0.1',
-                    'vrouter_port':12326,
-                    'integratemgmt':True,
+                    'mountpoint_bfs': '/mnt/bfs',
+                    'mountpoint_temp': '/mnt/tmp',
+                    'mountpoint_md': '/mnt/md',
+                    'mountpoint_readcaches': ['/mnt/cache1'],
+                    'mountpoint_writecaches': ['/mnt/cache2'],
+                    'mountpoint_foc': '/mnt/cache1',
+                    'storage_ip': '127.0.0.1',
+                    'vrouter_port': 12326,
+                    'integratemgmt': True,
                     'connection_backend': {},
-                    'connection_password':'',
-                    'connection_username':'',
-                    'connection_host':'',
-                    'connection_port':12326,
+                    'connection_password': '',
+                    'connection_username': '',
+                    'connection_host': '',
+                    'connection_port': 12326,
                     'config_params': {'dtl_mode': 'sync',
                                       'sco_size': 4,
                                       'dedupe_mode': 'dedupe',
@@ -65,4 +70,4 @@ add_vpool_params = {'storagerouter_ip':IP,
                                       'dtl_transport': 'tcp',
                                       }
                     }
-StorageRouterController.add_vpool.apply_async(kwargs={'parameters':add_vpool_params}).get(timeout=300)
+StorageRouterController.add_vpool.apply_async(kwargs={'parameters': add_vpool_params}).get(timeout=300)
